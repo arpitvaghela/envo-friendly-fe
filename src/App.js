927bea299fa52250f199a03360fa31d57cc8044d
Arpitsinh Vaghela <arpitvaghela9210@gmail.com>
2020-02-08 20:37:26 +0530

    added product listing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+import Sidebar from './components/sidebar';
+import Navbar from './components/navbar'
+import Card from './components/card'
+import image from './images/image.jpg'
 
+const menu = [{ name: 'A', linkpath: '/L1' }, { name: 'B', linkpath: '/L2' }]
+const card = { image: image, e_index:5,price:120,aboutus:"This product is something"}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cardlist">
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
+      <Card card={card} />
     </div>
   );
 }
